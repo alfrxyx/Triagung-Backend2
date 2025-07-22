ed9c2569e91219f1ef402ce6d241a100fc215542
alfrxyx <alfarabigajali@gmail.com>
2025-07-22 20:24:11 +0700

    Update application to use port 8080 instead of 8000 across configuration files

--- config/server.php
+++ config/server.php
@@ -11,7 +11,7 @@ return [
     |
     */
 
-    'port' => env('PORT', 8000),
+    'port' => env('PORT', 8080),
     'host' => env('HOST', '0.0.0.0'),
 
     /*
@@ -24,7 +24,7 @@ return [
     */
 
     'dev_server' => [
-        'port' => 8000,
+        'port' => 8080,
         'host' => '0.0.0.0',
     ],
 
@@ -38,7 +38,7 @@ return [
     */
 
     'production_server' => [
-        'port' => 8000,
+        'port' => 8080,
         'host' => '0.0.0.0',
     ],
 ];
